b091cba944c0e503e7c5bfbc3c6f140d7b0924a1
Ekaterina Miniyanova <katuxa@yandex.ru>
2020-10-13 21:18:29 +0500

    Создает компоненты App, экран приветствия, валидация props

---
+++ src/components/app/app.jsx
@@ -0,0 +1,16 @@
+import WelcomeScreen from "../welcome-screen/welcome-screen";
+
+const App = (props) => {
+  const {errorsCount} = props;
+
+  return (
+    <WelcomeScreen errorsCount={errorsCount} />
+  );
+};
+
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired
+};
+
+export default App;
+
